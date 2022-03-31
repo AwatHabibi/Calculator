f7ae125526bfb2eb5f89f8253bab103d0ecf38ba
Alex Habibi <AwatHabibi@icloud.com>
2022-03-30 22:10:57 -0400

    Add Calculator funtion, changed parseInt to ParseFloat

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseFloat(prompt("enter a number"));
+const number2 = parseFloat(prompt("Enter another number"));
+const result = add(number1, number2);
+alert(result);
