fc6d20282909ac79b7a7207712524b5b7bf803ed
Alex Habibi <AwatHabibi@icloud.com>
2022-03-30 22:19:58 -0400

    Change function to divide

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-function multiply(number1, number2) {
-  return number1 * number2;
+function divide(number1, number2) {
+  return number1 / number2;
 }
 
 const number1 = parseFloat(prompt("enter a number"));
 const number2 = parseFloat(prompt("Enter another number"));
-const result = multiply(number1, number2);
+const result = divide(number1, number2);
 alert(result);
